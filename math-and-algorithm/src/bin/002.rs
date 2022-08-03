b3b3b3358378e13c08dacdec8b6c4959f19a8139
shoya <cook.shoya@gmail.com>
2022-08-03 19:24:50 +0900

    003

--- math-and-algorithm/src/bin/002.rs
+++ math-and-algorithm/src/bin/002.rs
@@ -2,5 +2,10 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a1: i32,
+        a2: i32,
+        a3: i32,
+    }
+    println!("{}", a1 + a2 + a3)
 }
--- math-and-algorithm/src/bin/003.rs
+++ math-and-algorithm/src/bin/003.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+        a: [i32; n],
+    }
+
+    let mut res: i32 = 0;
+
+    for i in 0..n {
+        let element: i32 = a[i as usize];
+        res += element;
+    }
+    println!("{}", res)
 }
