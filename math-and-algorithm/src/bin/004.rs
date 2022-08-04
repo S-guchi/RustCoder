3d4105a3c0f5cd865dbab940b7c7fb38dd58b039
shoya <cook.shoya@gmail.com>
2022-08-05 07:46:23 +0900

    004

--- math-and-algorithm/src/bin/004.rs
+++ math-and-algorithm/src/bin/004.rs
@@ -2,5 +2,12 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a1:i32,
+        a2:i32,
+        a3:i32,
+    }
+
+    let res: i32 = a1 * a2 * a3;
+    println!("{}", res)
 }
